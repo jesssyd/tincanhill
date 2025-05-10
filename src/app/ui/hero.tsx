dc996b5693638d279faeb426e3d319a9d6c6ccd9
jesssyd <tidd.jessica.s@gmail.com>
2025-05-09 23:27:02 -0400

    hero section

--- src/app/ui/hero.tsx
+++ src/app/ui/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
         <div className="hero-content lg:grid grid-cols-12 lg:gap-6 lg:px-8 relative z-10 items-center md:items-start text-neutral-content text-center lg:text-left">
           {/* <div className="hero flex justify-center lg:justify-start min-h-screen bg-[url(https://static.wixstatic.com/media/b70c51_eee1426fbfff40b3bb5314540ca9379c~mv2.jpg/v1/fill/w_1960,h_1526,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/b70c51_eee1426fbfff40b3bb5314540ca9379c~mv2.jpg)]">
         <div className="hero-content items-center md:items-start text-neutral-content text-center lg:text-left"> */}
-          <div className="col-start-2 col-end-auto w-md lg:max-w-lg lg:flex flex-col items-start text-base-content">
+          <div className="sm:col-start-2 col-end-auto min-w-2xs sm:w-md lg:max-w-lg lg:flex flex-col items-start text-base-content">
             <h1 className="text-5xl">
               Preserving a Natural Jewel in the Heart of Yellowknife
             </h1>
@@ -26,9 +26,3 @@ export default function Hero() {
   );
 }
 
-{
-  /* <div className="mt-8 flex justify-center sm:justify-start gap-4 text-center">
-<Button href="#" text="Get Started" transparent={false} />
-<Button href="#" text="Learn More" transparent={true} />
-</div> */
-}
