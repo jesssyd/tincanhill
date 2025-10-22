59fde53935bbcbcbf1effda8a026b18ed1e75ec9
jesssyd <tidd.jessica.s@gmail.com>
2025-10-22 10:43:00 -0400

    Temporarily disable events page :(

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -22,20 +22,24 @@ export default async function Events() {
         <div className="flex flex-col items-center">
           <div className="w-full max-w-[800px]">
             <h2 className="text-left">
-              Upcoming Events
+              Under construction! Please check back soon.
             </h2>
+            {/* <h2 className="text-left">
+              Upcoming Events
+            </h2> */}
 
             <div className="relative w-full pt-[75%]">
               {" "}
               {/* 4:3 aspect ratio */}
-              <iframe
+              
+              {/* <iframe
                 src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FEdmonton&showPrint=0&showNav=0&mode=AGENDA&showTz=0&title=TCHCC%20Events&src=dGluY2FuaGlsbHllbGxvd2tuaWZlQGdtYWlsLmNvbQ&color=%23039BE5"
                 className="absolute top-0 left-0 w-full h-full"
                 frameBorder="0"
                 scrolling="no"
                 title="TCHCC Events"
                 allowFullScreen
-              ></iframe>
+              ></iframe> */}
             </div>
           </div>
         </div>
